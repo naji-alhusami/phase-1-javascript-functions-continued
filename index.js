00a062d1026127287b30795c8d823cce78882290
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-10 13:32:03 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const saturdayFun = (activity = "roller-skate") => {
+  return `This Saturday, I want to ${activity}!`;
+};
+console.log(saturdayFun());
+
+const mondayWork = (activity = "go to the office") => {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork());
+
+function wrapAdjective(result = "*") {
+  return function (emphatic = "a hard worker") {
+    return `You are ${result}${emphatic}${result}!`;
+  };
+}
+
+console.log(wrapAdjective()());
